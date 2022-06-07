4fa3a2671c7d6e0b79f5f150e5a004d7ddfc1c41
Yasmin <yasmin.hammoud@correo.unimet.edu.ve>
2022-06-07 14:16:59 -0400

    scrollToTop applied

--- src/Rutas/Rutas.jsx
+++ src/Rutas/Rutas.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+
 import { Store } from "../components/Store/Store";
-import { Checkout } from "../components/Checkout";
 import { Landingpage } from "../components/Landingpage/Landingpage";
 import { AboutUs } from "../components/AboutUs/AboutUs";
 import { CartMax } from "../components/CartMax/CartMax";
@@ -10,20 +11,20 @@ import { ProductosFilter } from "../components/Productos/ProductosFilter";
 export const Rutas = () => {
   return (
     <>
-      <Routes>
-        <Route index element={<Landingpage />} />
-        <Route path="/store/filters" element={<ProductosFilter />} />
+      <ScrollToTop>
+        <Routes>
+          <Route index element={<Landingpage />} />
+          <Route path="/store/filters" element={<ProductosFilter />} />
 
-        <Route path="/store" element={<Store />} />
+          <Route path="/store" element={<Store />} />
 
-        <Route path="/store/c/:category" element={<Store />} />
+          <Route path="/store/c/:category" element={<Store />} />
 
-        <Route path="/carrito" element={<CartMax />} />
-        <Route path="/pedido" element={<Checkout />} />
+          <Route path="/carrito" element={<CartMax />} />
 
-        <Route path="/quienes-somos" element={<AboutUs />} />
-
-      </Routes>
+          <Route path="/quienes-somos" element={<AboutUs />} />
+        </Routes>
+      </ScrollToTop>
     </>
   );
 };

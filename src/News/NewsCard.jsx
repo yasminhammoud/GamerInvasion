5a66326770767c1bce203f4964b6b84aca3a61aa
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2022-06-10 20:13:32 -0400

    tarjetas

--- src/News/NewsCard.jsx
+++ src/News/NewsCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import './cards.css';
 
-function NewsCard() {
+function NewsCard(props) {
+  console.log(props)
   return (
-    <div className='card'>
-        <img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/7/10/enhanced/webdr09/original-11165-1428416151-24.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto' alt='' />
-        <div className='card-body'>
-            <h4 className='card-title'>Titulo</h4>
-            <p className='card-text text-secondary'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam nihil sit quas officiis minima 
-                laboriosam cum aliquam quia officia error architecto praesentium beatae impedit blanditiis, 
-                ducimus pariatur accusantium? Iure, perspiciatis?</p>
+    <div className='card text-center bg-dark '>
+        <div className='overflow'>
+          <img src={props.imgURL} alt='image' className='card-img-top' id='image'/>
+        </div>
+        <div className='card-body text-light' id='card-body'>
+            <a href = {props.url} target = 'blank' className='card-title'><h4>{props.title}</h4></a>
+            <p className='card-text text-secondary'>{props.description}?</p>
+            <p className='card-text text-secondary'>Fuente: {props.source}</p>
         </div>
         
     </div>
--- src/News/NewsGrid.jsx
+++ src/News/NewsGrid.jsx
@@ -1,19 +1,87 @@
+import { faCarSide } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import NewsCard from './NewsCard';
+
+const news = [
+    {
+        id: 1,
+        title:'Titulo noticia 1',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/08/Social-Media-Video-Specs-Feature-Image.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.google.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 2,
+        title: 'Titulo noticia 2',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/08/Social-Media-Video-Specs-Feature-Image.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.youtube.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 3,
+        title:'Titulo noticia 3',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.google.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 4,
+        title: 'Titulo noticia 4',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.google.com',
+        source: 'Gameranx',
+    }, 
+    {
+        id: 5,
+        title:'Titulo noticia 5',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.youtube.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 6,
+        title: 'Titulo noticia 6',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.youtube.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 7,
+        title: 'Titulo noticia 7',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.youtube.com',
+        source: 'Gameranx',
+    },
+    {
+        id: 8,
+        title: 'Titulo noticia 8',
+        imageURL: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero temporibus ea accusantium consectetur, nemo iusto quo eum esse repellendus pariatur alias. Excepturi saepe soluta minima autem, harum recusandae iusto laboriosam.',
+        url: 'https://www.youtube.com',
+        source: 'Gameranx',
+    }
+]
 
 function NewsGrid() {
   return (
-    <div className='container'>
+    <div className='container d-flex justify-content-center border-0'>
         <div className='row'>
-            <div className='col-md-4'>
-                <NewsCard />
-            </div>
-            <div className='col-md-4'>
-                <NewsCard />
-            </div>
-            <div className='col-md-4'>
-                <NewsCard />
-            </div>
+            
+            {
+                news.map(new1 =>(
+                    <div className='col-md-4' key={new1.id}>
+                        <NewsCard title={new1.title} imgURL = {new1.imageURL} description = {new1.description} url = {new1.url} source = {new1.source}/>
+                    </div>
+                ))
+            }
         </div>
         
     </div>

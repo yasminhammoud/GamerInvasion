a5ad7095f11a83a964edf032952910c5d1e1e055
Fidel <fidel.barreat@nhubex.com>
2022-06-12 20:15:03 -0400

    bootstrap al detalle

--- src/components/Productos/ProductDetail.jsx
+++ src/components/Productos/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row, Col, Container, CloseButton } from "react-bootstrap";
+import { Button, Row, Col, Container, CloseButton, Image } from "react-bootstrap";
 import { useContext, useState, useEffect } from "react";
 import {useParams } from 'react-router-dom';
 import{
@@ -44,18 +44,34 @@ export const ProductDetail = () => {
 
   return (
     <>
-      <div>
-        <h1>{formProducto.nombre}</h1>
-        <h3>{formProducto.marca}</h3>
-        <h3>{formProducto.precio}</h3>
-        <h3>{formProducto.cantidad}</h3>
-        <h3>{formProducto.descripcion}</h3>
-        <br></br>
-        <h3>Especificaciones:</h3>
-        <h3>Dimensiones: {formProducto.especificaciones.Dimensiones}</h3>
-        <h3>Peso: {formProducto.especificaciones.Peso}</h3>
-        <h3>Ciudad de origen: {formProducto.especificaciones.CiudadOrigen}</h3>
-      </div>
+      <Container className="align-items-center" style={{padding: '4rem 1rem 4rem 1rem', background: 'lightgrey', borderRadius: '5px'}}>
+        <Row>
+          <Col xs={12} sm={6} className="text-center justify-content-center">
+            <img
+              src={fotos}
+              alt={formProducto.Nombre}
+              width="70%"
+            />
+          </Col>
+          <Col xs={12} sm={6}>
+            <h1>{formProducto.nombre}</h1>
+            <h3>{formProducto.marca}</h3>
+            <h3>${formProducto.precio}</h3>
+            <h4>{formProducto.descripcion}</h4>
+          </Col>
+        </Row>
+        <Row style={{margin: '5rem'}}>
+          <Col xs={12} sm={12}>
+            <h3>Especificaciones:</h3>
+            <h4>Dimensiones: {formProducto.especificaciones.Dimensiones}</h4>
+            <h4>Peso: {formProducto.especificaciones.Peso}</h4>
+            <h4>Ciudad de origen: {formProducto.especificaciones.CiudadOrigen}</h4>
+          </Col>
+        </Row>
+        <Row>
+          aca va productos sugeridos
+        </Row>
+      </Container>
     </>
     );
 };

dec044165d7310771298fa5516d9e3d5bc4ff86f
AlejandroGuzman44 <alejandro.guzman@correo.unimet.edu.ve>
2022-06-21 15:44:31 -0400

    Arreglando contextoCarrito
    Aca se arreglo un error que daba el contexto del carrito

--- src/contexts/ContextoCarrito.jsx
+++ src/contexts/ContextoCarrito.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState, createContext } from "react";
 export const ContextoCarrito = createContext();
 
 export const ContextoCarritoProvider = ({ children }) => {
-  
+
   // Aca lo que se hace es guardar los productos que se encuentran en el carrito en el local storage 
-    // Para luego retornarlo y tener asi de esa forma un carrito con todos los productos que la persona agrego
-  
+  // Para luego retornarlo y tener asi de esa forma un carrito con todos los productos que la persona agrego
+
   const [productoCarrito, setproductoCarrito] = useState(() => {
     try {
       const productosLocalStorage = localStorage.getItem("productosCarrito");
@@ -15,19 +15,19 @@ export const ContextoCarritoProvider = ({ children }) => {
       return [];
     }
   });
-  
+
   // Este useEffect lo que hace es setear los procutos que estan en el carrito dentro de productoCarrito 
-    // que se declaro en el useState
+  // que se declaro en el useState
 
   useEffect(() => {
     localStorage.setItem("productosCarrito", JSON.stringify(productoCarrito));
     // console.log(productoCarrito);
   }, [productoCarrito]);
-  
+
   // Con esta funcion lo que se hace es poder agregar productos al carrito , a esta funcion se le pasa como parametro
-    // el producto y luego de esto se hace un find para verificar si ese producto esta o no esta en el carrito 
-    // y en el caso de que este en el carrito lo que se hace es devolver el mismo producto pero se le suma en 1 unidad
-    // y si no esta en el carrito se agrega el producto en 1 cantidad
+  // el producto y luego de esto se hace un find para verificar si ese producto esta o no esta en el carrito 
+  // y en el caso de que este en el carrito lo que se hace es devolver el mismo producto pero se le suma en 1 unidad
+  // y si no esta en el carrito se agrega el producto en 1 cantidad
 
   const agregarProductoCarrito = (producto) => {
     const enCarrito = productoCarrito.find(
@@ -51,10 +51,10 @@ export const ContextoCarritoProvider = ({ children }) => {
   };
 
   // Con esta funcion lo que se hace es poder eliminar productos al carrito , a esta funcion se le pasa como parametro
-    // el producto y luego de esto se hace un find para verificar si ese producto esta o no esta en el carrito 
-    // y en el caso de que este en el carrito y este una vez se le decrementa en 1 unidad dicho producto
-    // y si no esta en el carrito pues simplemente se deja de esa manera.
- 
+  // el producto y luego de esto se hace un find para verificar si ese producto esta o no esta en el carrito 
+  // y en el caso de que este en el carrito y este una vez se le decrementa en 1 unidad dicho producto
+  // y si no esta en el carrito pues simplemente se deja de esa manera.
+
   const eliminarProductoCarrito = (producto) => {
     const enCarrito = productoCarrito.find(
       (productoEnCarrito) => productoEnCarrito.id === producto.id
@@ -78,8 +78,8 @@ export const ContextoCarritoProvider = ({ children }) => {
   };
 
   // Con esta funcion lo que se hace es darle la facilidad al usuario para que pueda eliminar todo el producto con un solo click
-    // para que de esta manera no tenga que sacarlo 1 por 1
-  
+  // para que de esta manera no tenga que sacarlo 1 por 1
+
   const eliminarTodoProducto = (producto) => {
     setproductoCarrito(
       productoCarrito.filter(
@@ -99,4 +99,5 @@ export const ContextoCarritoProvider = ({ children }) => {
     >
       {children}
     </ContextoCarrito.Provider>
-  );
+  )
+}
